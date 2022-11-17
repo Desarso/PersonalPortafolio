655be85ca629e842526db92f98c1813ff315dd26
Desarso <malek.gabriel33@gmail.com>
2022-11-17 19:41:00 +0000

    Deploying to public from @ Desarso/PersonalPortafolio@7340a946b546384f62a0edabd796c1bec19b28b8 🚀

--- _next/static/iRbgT8oC1ARh3xK1yw125/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/85-516fea3005639ee1.js","static/chunks/pages/index-cf301d8474c4cba3.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
